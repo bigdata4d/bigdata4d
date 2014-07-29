51d29cc834df74c27da0ee3041784ad830872708
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2014-07-29 22:24:08 +0200

    Running benchmark GPU vs CPU

--- examples/timer_cuda/timer_cuda.cxx
+++ examples/timer_cuda/timer_cuda.cxx
@@ -7,8 +7,9 @@
 #include "Timer.h"
 
 // CUDA functions
-extern void prepareCUDA(float *&, float *&, float *, float *);
+extern void prepareCUDA(float **, float **, float *, float *);
 extern void testCUDA(float *, float *);
+extern void waitCUDA();
 extern void finishCUDA(float *, float *, float *);
 
 int main(int argc, char *argv[])
@@ -19,41 +20,42 @@ int main(int argc, char *argv[])
     Grid grid(master, 100, 100, 100);
 
     Field<float> a(master, grid, "a");
-    Field<float> b(master, grid, "b");
 
     // fill field with random numbers
     for(auto &i : a.data)
       i = std::rand() % 10;
 
-    for(auto &i : b.data)
-      i = std::rand() % 10;
+    // make a copy of a
+    Field<float> b(a);
 
     Timer timer1(master);
     timer1.start();
-    for(int n=0; n<1000; ++n)
+    for(int n=0; n<100; ++n)
       a += b;
     timer1.end();
 
     float *a_gpu, *b_gpu;
-    prepareCUDA(a_gpu, b_gpu, &a.data[0], &b.data[0]);
+    prepareCUDA(&a_gpu, &b_gpu, &a.data[0], &b.data[0]);
 
     Timer timer2(master);
     timer2.start();
-    for(int n=0; n<1000; ++n)
+    for(int n=0; n<100; ++n)
       testCUDA(a_gpu, b_gpu);
+    waitCUDA();
     timer2.end();
 
     std::ostringstream message;
     message << "Elapsed time (s): "
             << std::setprecision(5) << timer1.getTotal() << ", "
-            << std::setprecision(5) << timer2.getTotal() << "\n";
+            << std::setprecision(5) << timer2.getTotal() << "\n"
+            << "Speedup: " << timer1.getTotal() / timer2.getTotal() << "\n";
     master.printMessage(message.str());
 
     Field<float> acuda(master, grid, "acuda");
 
     finishCUDA(a_gpu, b_gpu, &acuda.data[0]);
 
-    for(int n=0; n<a.data.size(); n+=100*100*10)
+    for(int n=3; n<a.data.size(); n+=100*100*10)
     {
       std::ostringstream message;
       message << std::setw(8);
