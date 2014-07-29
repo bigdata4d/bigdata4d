20954618b599bcb76ea2378e1ffd2ddcac5e10c2
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2014-07-29 23:27:10 +0200

    Working SUPERSLOW version

--- examples/timer_cuda/timer_cuda.cu
+++ examples/timer_cuda/timer_cuda.cu
@@ -1,19 +1,24 @@
+#include <cstdio>
+
 __global__ void compadd_kernel(float *a, float *b)
 {
   int i = blockIdx.x*blockDim.x + threadIdx.x;
   int j = blockIdx.y*blockDim.y + threadIdx.y;
-  int k = blockIdx.z;
-  int ijk = i + j*100 + k * 100*100;
 
-  a[ijk] += b[ijk];
+  int ij = i + j*10;
+  a[ij] += b[ij];
+
+  // int k = blockIdx.z;
+  // int ijk = i + j*10 + k*100;
+  // a[ijk] += b[ijk];
 }
 
 void prepareCUDA(float **a_gpu, float **b_gpu, float *a, float *b)
 {
-  int size = 100*100*100*sizeof(float);
+  int size = 10*10*10*sizeof(float);
 
   cudaMalloc((void **)a_gpu, size);
-  cudaMalloc((void **)a_gpu, size);
+  cudaMalloc((void **)b_gpu, size);
 
   cudaMemcpy(*a_gpu, a, size, cudaMemcpyHostToDevice);
   cudaMemcpy(*b_gpu, b, size, cudaMemcpyHostToDevice);
@@ -21,8 +26,8 @@ void prepareCUDA(float **a_gpu, float **b_gpu, float *a, float *b)
 
 void testCUDA(float *a_gpu, float *b_gpu)
 {
-  dim3 grid(100,100,100);
-  dim3 block(1,1,1);
+  dim3 grid(10,100);
+  dim3 block(1,1);
 
   compadd_kernel<<<grid, block>>>(a_gpu, b_gpu);
 }
@@ -34,7 +39,7 @@ void waitCUDA()
 
 void finishCUDA(float *a_gpu, float *b_gpu, float *a)
 {
-  int size = 100*100*100*sizeof(float);
+  int size = 10*10*10*sizeof(float);
 
   cudaMemcpy(a, a_gpu, size, cudaMemcpyDeviceToHost);
 
