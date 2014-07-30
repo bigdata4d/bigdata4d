3efe743d6a520b07c243e80de9169db04b952cfc
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2014-07-30 19:37:20 +0200

    Changed the settings slightly

--- config/macbook.cmake
+++ config/macbook.cmake
@@ -1,9 +1,20 @@
 # MacBook
-set(ENV{CXX} mpicxx) # compiler for parallel build
+#set(ENV{CXX} mpicxx)
+set(ENV{CC}  clang  )
+set(ENV{CXX} clang++)
 
-set(USER_CXX_FLAGS "-std=c++11")
+set(USER_CXX_FLAGS "-stdlib=libstdc++ -std=c++11")
 set(USER_CXX_FLAGS_RELEASE "-O3 -ffast-math -mtune=native -march=native")
 set(USER_CXX_FLAGS_DEBUG "-O0 -g -Wall -Wno-unknown-pragmas")
+
+set(USER_C_FLAGS "-stdlib=libstdc++")
+set(USER_C_FLAGS_RELEASE "-O3 -ffast-math -mtune=native -march=native")
+set(USER_C_FLAGS_DEBUG "-O0 -g -Wall -Wno-unknown-pragmas")
+
+if(USECUDA)
+  set(CUDA_PROPAGATE_HOST_FLAGS OFF)
+  set(CUDA_NVCC_FLAGS "-arch=sm_13")
+endif()
 
 add_definitions(-DRESTRICTKEYWORD=__restrict__)
 
--- examples/timer_cuda/timer_cuda.cxx
+++ examples/timer_cuda/timer_cuda.cxx
@@ -26,85 +26,94 @@ int main(int argc, char *argv[])
   try
   {
     Master master;
-    Grid grid(master, 384, 384, 384);
-
-    Field<float> a(master, grid, "a");
-
-    // fill field with random numbers
-    for(auto &i : a.data)
-      i = std::rand() % 10;
-
-    // make a copy of a
-    Field<float> b(a);
-
-    Timer timer1(master);
-    timer1.start();
-    for(int n=0; n<100; ++n)
-      a += b;
-    timer1.end();
-
-    float *a_gpu, *b_gpu;
-    prepareCUDA(&a_gpu, &b_gpu, &b.data[0], &b.data[0]);
-
-    Timer timer2(master);
-    timer2.start();
-    for(int n=0; n<100; ++n)
-      testCUDA(a_gpu, b_gpu);
-    waitCUDA();
-    timer2.end();
-
-    Field<float> acuda(master, grid, "acuda");
-    finishCUDA(a_gpu, b_gpu, &acuda.data[0]);
-
-    // THRUST
-    thrust::device_vector<float> athrust(b.data.begin(), b.data.end());
-    thrust::device_vector<float> bthrust(b.data.begin(), b.data.end());
-
-    Timer timer3(master);
-    timer3.start();
-    for(int n=0; n<100; ++n)
-      testCUDA_thrust(athrust, bthrust);
-    waitCUDA();
-    timer3.end();
-
-    Field<float> athrustout(master, grid, "athrustout");
-    thrust::copy(athrust.begin(), athrust.end(), athrustout.data.begin());
-
-    // cuBLAS
-    prepareCUDA_cublas(&a_gpu, &b_gpu, &b.data[0], &b.data[0]);
-
-    Timer timer4(master);
-    timer4.start();
-    for(int n=0; n<100; ++n)
-      testCUDA_cublas(a_gpu, b_gpu);
-    waitCUDA();
-    timer4.end();
-
-    Field<float> acublas(master, grid, "acublas");
-    finishCUDA_cublas(a_gpu, b_gpu, &acublas.data[0]);
-
-    std::ostringstream message;
-    message << "Elapsed time (s): "
-            << std::setprecision(5) << timer1.getTotal() << ", "
-            << std::setprecision(5) << timer2.getTotal() << ", "
-            << std::setprecision(5) << timer3.getTotal() << ", "
-            << std::setprecision(5) << timer4.getTotal() << "\n"
-            << "Speedup CUDA (no thrust): " << timer1.getTotal() / timer2.getTotal() << "\n"
-            << "Speedup CUDA (thrust)   : " << timer1.getTotal() / timer3.getTotal() << "\n"
-            << "Speedup CUDA (cublas)   : " << timer1.getTotal() / timer4.getTotal() << "\n";
-    master.printMessage(message.str());
-
-    for(int n=3; n<a.data.size(); n+=384*384*20)
+    try
     {
+      Grid grid(master, 384, 384, 384);
+
+      Field<float> a(master, grid, "a");
+
+      // fill field with random numbers
+      for(auto &i : a.data)
+        i = std::rand() % 10;
+
+      // make a copy of a
+      Field<float> b(a);
+
+      Timer timer1(master);
+      timer1.start();
+      for(int n=0; n<100; ++n)
+        a += b;
+      timer1.end();
+
+      float *a_gpu, *b_gpu;
+      prepareCUDA(&a_gpu, &b_gpu, &b.data[0], &b.data[0]);
+
+      Timer timer2(master);
+      timer2.start();
+      for(int n=0; n<100; ++n)
+        testCUDA(a_gpu, b_gpu);
+      waitCUDA();
+      timer2.end();
+
+      Field<float> acuda(master, grid, "acuda");
+      finishCUDA(a_gpu, b_gpu, &acuda.data[0]);
+
+      // THRUST
+      thrust::device_vector<float> athrust(b.data.begin(), b.data.end());
+      thrust::device_vector<float> bthrust(b.data.begin(), b.data.end());
+
+      Timer timer3(master);
+      timer3.start();
+      for(int n=0; n<100; ++n)
+        testCUDA_thrust(athrust, bthrust);
+      waitCUDA();
+      timer3.end();
+
+      Field<float> athrustout(master, grid, "athrustout");
+      thrust::copy(athrust.begin(), athrust.end(), athrustout.data.begin());
+
+      // cuBLAS
+      prepareCUDA_cublas(&a_gpu, &b_gpu, &b.data[0], &b.data[0]);
+
+      Timer timer4(master);
+      timer4.start();
+      for(int n=0; n<100; ++n)
+        testCUDA_cublas(a_gpu, b_gpu);
+      waitCUDA();
+      timer4.end();
+
+      Field<float> acublas(master, grid, "acublas");
+      finishCUDA_cublas(a_gpu, b_gpu, &acublas.data[0]);
+
       std::ostringstream message;
-      message << std::setw(8);
-      message << n << " = {" 
-        << std::setw(6) <<          b.data[n] << ", "
-        << std::setw(6) <<          a.data[n] << ", "
-        << std::setw(6) <<      acuda.data[n] << ", "
-        << std::setw(6) << athrustout.data[n] << ", "
-        << std::setw(6) <<    acublas.data[n] << " }\n";
+      message << "Elapsed time (s): "
+              << std::setprecision(5) << timer1.getTotal() << ", "
+              << std::setprecision(5) << timer2.getTotal() << ", "
+              << std::setprecision(5) << timer3.getTotal() << ", "
+              << std::setprecision(5) << timer4.getTotal() << "\n"
+              << "Speedup CUDA (no thrust): " << timer1.getTotal() / timer2.getTotal() << "\n"
+              << "Speedup CUDA (thrust)   : " << timer1.getTotal() / timer3.getTotal() << "\n"
+              << "Speedup CUDA (cublas)   : " << timer1.getTotal() / timer4.getTotal() << "\n";
       master.printMessage(message.str());
+
+      for(int n=3; n<a.data.size(); n+=384*384*20)
+      {
+        std::ostringstream message;
+        message << std::setw(8);
+        message << n << " = {" 
+          << std::setw(6) <<          b.data[n] << ", "
+          << std::setw(6) <<          a.data[n] << ", "
+          << std::setw(6) <<      acuda.data[n] << ", "
+          << std::setw(6) << athrustout.data[n] << ", "
+          << std::setw(6) <<    acublas.data[n] << " }\n";
+        master.printMessage(message.str());
+      }
+    }
+    
+    catch (...)
+    {
+      master.printError("Oops! An unknown error has occured!\n");
+      throw 1;
     }
   }
 
