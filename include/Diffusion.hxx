46b28ab154bbbf63babb6cd122e43efca5039846
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2015-01-19 16:55:53 +0100

    Renamed the project

--- include/Diffusion.hxx
+++ include/Diffusion.hxx
@@ -1,24 +1,24 @@
 /*
- * BigData4D
+ * BigDataGrid
  * Copyright (c) 2014 Chiel van Heerwaarden
  *
- * Many of the classes and functions in BigData4D are derived from
+ * Many of the classes and functions in BigDataGrid are derived from
  * MicroHH (https://github.com/MicroHH)
  *
- * This file is part of BigData4D
+ * This file is part of BigDataGrid
  *
- * BigData4D is free software: you can redistribute it and/or modify
+ * BigDataGrid is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
 
- * BigData4D is distributed in the hope that it will be useful,
+ * BigDataGrid is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
 
  * You should have received a copy of the GNU General Public License
- * along with BigData4D.  If not, see <http://www.gnu.org/licenses/>.
+ * along with BigDataGrid.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "Diffusion.h"
